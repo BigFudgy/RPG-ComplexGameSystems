661fe97df32c9e958888feec97ebf19c842b7bab
Harry Fuge <harry.fuge@gmail.com>
2021-05-14 15:32:36 +1000

    Modifying

---
+++ RPG Systems/Assets/Scripts/AbilityCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public static class AbilityCreator {
+
+    [MenuItem("Assets/Create/Create New Ability")]
+    public static void CreateNewAbility() 
+    {
+        CharacterStats stats = ScriptableObject.CreateInstance<CharacterStats>();
+        ProjectWindowUtil.CreateAsset(stats, "NewAbility.asset");
+    }
+
+}
