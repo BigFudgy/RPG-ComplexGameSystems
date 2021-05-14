ec91d8d754825967ac53ce5ae2636ab288546eb9
Harry Fuge <harry.fuge@gmail.com>
2021-05-14 14:59:08 +1000

    Beginning To use ScriptableObjects(SO)

--- RPG Systems/Assets/Scripts/CharacterStats.cs
+++ RPG Systems/Assets/Scripts/CharacterStats.cs
@@ -2,21 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterStats : MonoBehaviour
-{
+public class CharacterStats : ScriptableObject {
 
     public float health;
     public int stamina, mana, defense, resistance;
-    public int strength, dexterity, constitution, intellect, luck;
+    public int strength, dexterity, constitution, intellect, luck, exp;
 
+   
 
-
-    [System.Serializable]
-    public class CombatStats
-    {
-        public float attackPower;
-        public float defense;
-        public float dodge;
-    }
 }
  
