9f171f2226bc507daa6d42ee6125184ccd2cd66e
Harry Fuge <harry.fuge@gmail.com>
2021-05-21 16:22:25 +1000

    Modifying Scripts and setting up skills + map change

---
+++ RPG Systems/Assets/Scripts/BaseStatCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class BaseStatCreator  
+{
+
+    public static void CreateNewBaseStats() 
+    {
+        CharacterStats stats = ScriptableObject.CreateInstance<CharacterStats>();
+        ProjectWindowUtil.CreateAsset(stats, "BaseStats.asset");
+    }
+
+}
--- RPG Systems/Assets/Scripts/CharacterStats.cs
+++ RPG Systems/Assets/Scripts/CharacterStats.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "NewCharacterStats", menuName = "RPG Systems/Stats/Create New BaseStats", order = 1)]
 
 public class CharacterStats : ScriptableObject {
 
+    public string charName;
     public float health;
     public int stamina, mana, defense, resistance;
     public int strength, dexterity, constitution, intellect, luck;
