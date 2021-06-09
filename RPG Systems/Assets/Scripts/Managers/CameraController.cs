c4f6b1d326379b17579f0cacf803ef2b555ba975
Harry Fuge <harry.fuge@gmail.com>
2021-06-10 09:05:45 +1000

    Updated Camera

--- RPG Systems/Assets/Scripts/Managers/CameraController.cs
+++ RPG Systems/Assets/Scripts/Managers/CameraController.cs
@@ -13,52 +13,25 @@ public class CameraController : MonoBehaviour
 	public float maxZoom = 3f;
 	public float minZoom = .3f;
 
-	public float yawSpeed = 10f;
+	public float yawSpeed = 100f;
 	
 	private float currentZoom = 10f;
 	private float currentYaw = 0.0f;
-	private float yawInput = 0f;
 
 	public Quaternion newRotation;
-
 
 	 public void Update()
     {
 		currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
 		currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
-
-		//float horiRotate = Input.GetAxis("HoriRotate");
-		 
-		//if (Input.GetKey(KeyCode.Q))
-		//{
-		//	transform.rotation *= Quaternion.Euler(Vector3.up * -yawSpeed);
-		//
-		//}
-		//else if (Input.GetKey(KeyCode.E))
-		//{
-		//	transform.rotation *= Quaternion.Euler(Vector3.up * yawSpeed);
-		//
-		//}
-
 	}
-
 
     public void LateUpdate()
     {
 		transform.position = target.position - offset * currentZoom;
 		transform.LookAt(target.position + Vector3.up * pitch);
 
-		if (Input.GetKey(KeyCode.Q))
-		{
-			transform.rotation *= Quaternion.Euler(Vector3.up * -yawSpeed);
-		
-		}
-		else if (Input.GetKey(KeyCode.E))
-		{
-			transform.rotation *= Quaternion.Euler(Vector3.up * yawSpeed);
-		
-		}
-		//currentYaw -= Input.GetAxisRaw("Horizontal") * yawSpeed * Time.deltaTime;
-		//transform.RotateAround(target.position, Vector3.up, currentYaw);
+		currentYaw -= Input.GetAxisRaw("HoriRotate") * yawSpeed * Time.deltaTime;
+		transform.RotateAround(target.position, Vector3.up, currentYaw);
 	}
 }
